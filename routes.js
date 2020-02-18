878e21956db7944c5e8fb3c90b9fe6cebbb7840d
skrabacher <37253871+skrabacher@users.noreply.github.com>
2020-02-17 17:00:35 -0800

    added validation to all put/post routes

--- routes.js
+++ routes.js
@@ -26,22 +26,38 @@ function asyncHandler(cb){
 // *USER ROUTES*
 
 // GET /api/users 200 - Returns the currently authenticated user
-router.get('/users', (req, res) => {
-    const user = await User.findByPk(req.params.id);
-  });
+router.get('/users', asyncHandler(async (req, res) => {
+  const currentUser = PLACEHOLDER; //DRAFT*** need auth criteria
+  const authUser = await User.findByPk(currentUser.id); 
+    res.status(200).json(authUser);
+}));
 
+//VALIDATION CHAINS
 // check() returns a "validation chain". Any number of validation methods can be called on a validation chain to validate a field. 
-const nameValidationChain = check('name')
+const firstNameVC = check('firstName')
   .exists({ checkNull: true, checkFalsy: true })
-  .withMessage('Please provide a value for "name"');
+  .withMessage('Please provide a value for "firstName"');
+const lastNameVC = check('lastName')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "lastName"');
 // check() returns a "validation chain". Any number of validation methods can be called on a validation chain to validate a field. 
-const emailValidationChain = check('name')
+const emailVC = check('emailAddress')
   .exists({ checkNull: true, checkFalsy: true })
   .isEmail()
-  .withMessage('Please provide a value for "email"');
+  .withMessage('Please provide a value for "emailAddress"');
+const passwordVC = check('password')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "password"');
+const titleVC = check('title')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "title"');
+const descriptionVC = check('description')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "description"');
+
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
-router.post('/users', nameValidationChain, emailValidationChain, (req, res) => {
+router.post('/users', firstNameVC, lastNameVC, emailVC, passwordVC, asyncHandler(async (req, res) => {
     // Attempt to get the validation result from the Request object.
     const errors = validationResult(req); //validationResult extracts the validation errors from a request and makes them available in a Result object.
     // If there are validation errors...
@@ -51,39 +67,50 @@ router.post('/users', nameValidationChain, emailValidationChain, (req, res) => {
       // Return the validation errors to the client.
       res.status(400).json({ errors: errorMessages });
     } else {
-      // Get the user from the request body.
-      const user = req.body;
-  
-      // Add the user to the `users` array.
-      users.push(user);
-  
+      await User.create(req.body);//creates new instance of User model 
       // Set the status to 201 Created and end the response.
-      res.status(201).end();
+      res.status(201).location('/').end(); //sets response status & location header
     }
-  });
+}));
   
 // *COURSE ROUTES*
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
 router.get('/courses', asyncHandler(async (req, res) => {
-    
-  }));
+    const courses = await Course.findAll(); //DRAFT***
+    res.status(200).json(courses);
+}));
 // GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
 router.get('/courses/:id', asyncHandler(async (req, res) => {
-    
-  }));
+  const course = await Course.findByPk(req.params.id); //DRAFT***
+  res.status(200).json(course);
+}));  
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
-router.post('/courses', asyncHandler(async (req, res) => {
-    
-  }));
+router.post('/courses', titleVC, descriptionVC, asyncHandler(async (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req); //validationResult extracts the validation errors from a request and makes them available in a Result object.
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
+    // Return the validation errors to the client.
+    res.status(400).json({ errors: errorMessages });
+  } else {
+    await Course.create(req.body);//creates new instance of User model 
+    // Set the status to 201 Created and end the response.
+    res.status(201).location(PLACEHOLDER).end(); //sets response status & location header
+  }
+}));
 // PUT /api/courses/:id 204 - Updates a course and returns no content
 router.put('/courses/:id', asyncHandler(async (req, res) => {
-    
-  }));
+  
+  res.status(204).end();
+}));
 // DELETE /api/courses/:id 204 - Deletes a course and returns no content
 router.delete('/courses/:id', asyncHandler(async (req, res) => {
-    
-  }));
-   
+  
+  res.status(204).end();
+}));
+  
 module.exports = router;
    
    //***SJK NOTE*** DRAFT from here: C:\Users\Sarah\Documents\Treehouse Tech Degree\Unit 9\PRACTICE- data-relationships-with-sql-and-sequelize\starter-files\app.js
